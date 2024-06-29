b680b90d5f1c25f271b549d704840d9cb7599241
akash2885 <apuzhakkal414@gmail.com>
2024-06-29 18:25:57 -0400

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, ThemeProvider } from "@mui/material";
+import theme from "./components/palettestyle";
+import Navbar from "./components/navbar";
+import Content from "./components/Landing/content";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+  },
+  {
+    path: "/main",
+    element: <Content />,
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+      {/* <Navbar />
+      <Content /> */}
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
